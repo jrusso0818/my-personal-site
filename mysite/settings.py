49e80f6be3668329422725f0289d8c2fe7e7dc3f
jordanrusso <jrusso0818@gmail.com>
2020-12-18 16:24:27 -0800

    Added image attribute to project model. Created starting views & templates for project listing

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['projects/templates/projects'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'projects/project_list.html', context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
