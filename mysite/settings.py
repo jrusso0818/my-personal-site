20177819aace838af5a41ff9c7ed14f2b17e2c1d
jordanrusso <jrusso0818@gmail.com>
2020-12-21 21:06:17 -0800

    Added homepage, added project detail page, used bootstrap to clean things up. Fixed earlier static files issue.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'projects.apps.ProjectsConfig'
+    'projects.apps.ProjectsConfig',
+    'homepage.apps.HomepageConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['projects/templates/projects/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
