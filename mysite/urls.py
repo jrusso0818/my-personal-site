6b89dae4231a68f0bd2d88fcbe9d3cc4702adb27
jordanrusso <jrusso0818@gmail.com>
2020-12-22 18:02:37 -0800

    Added Pillow. Now projects can be added from admin view.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', include('homepage.urls')),
     path('projects/', include('projects.urls')),
@@ -23,4 +27,4 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
